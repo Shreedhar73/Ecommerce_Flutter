2a751387d4b9dca9f8a33e297c537bacb822d056
Shreedhar73 <shreedharpandey000@gmail.com>
2021-02-06 19:30:43 +0545

    Flutter Ecommerce start

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,9 +15,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+
   Widget build(BuildContext context) {
+
+    Widget imageCarousel = new Container(
+      height: 200.0,
+    );
+    Widget titleSection = new Container();
+
+
     return Scaffold(
       appBar: new AppBar(
+        elevation: 0.0,
         backgroundColor: Colors.blueGrey,
         title: Text('Shopify'),
         actions: <Widget>[
@@ -33,22 +43,104 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: new Drawer(
+
+
         child: new ListView(
+
+
           children: <Widget>[
+
             //header
             new UserAccountsDrawerHeader(
-                accountName: Text('Fallen700'),
+                accountName: Text('Shreedhar Pandeya',
+                style : TextStyle(
+                  color: Colors.white,
+                ), ),
                 accountEmail: Text('fa113n700@gmail.com'),
             currentAccountPicture: GestureDetector(
               child: new CircleAvatar(
                 backgroundColor: Colors.grey,
-                child: Icon(Icons.person, color: Colors.white,),
+                backgroundImage: AssetImage('images/2.jpg'),
+             //   child: Image(image:AssetImage('images/4.jpg')),
               ),
-            ),),
+            ),
+            decoration: new BoxDecoration(
+              color: Colors.blueGrey,
+            ),
+            ),
+
+            //body
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home, color: Colors.purple,),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My account'),
+                leading: Icon(Icons.person, color: Colors.purple,),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My orders'),
+                leading: Icon(Icons.shopping_bag_sharp, color: Colors.purple,),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.category, color: Colors.purple,),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Favourites'),
+                leading: Icon(Icons.favorite, color: Colors.purple,),
+              ),
+            ),
+
+            Divider(),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings, color: Colors.purple,),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help_rounded, color: Colors.purple,),
+              ),
+            ),
+
+
 
           ],
         ),
       ),
+
+      body: Column(
+        children: [
+          titleSection,
+
+        ],
+
+      ),
+
     );
   }
+
 }
